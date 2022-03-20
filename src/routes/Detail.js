117e86ca7452706bbd7dbd26f6f27d5f3ff80e34
Woo KyungJun <natcho9010@gmail.com>
2022-03-20 20:58:05 +0900

    Style: style of Home & Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
+import styled from 'styled-components';
 
-const ASDF = gql`
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(to right, #f03ab3, #e8781c);
+`;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  width: 60%;
+  color: white;
+`;
+
+const Description = styled.div`
+  width: 80%;
+  height: 60%;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+const SubDescription = styled.div`
+  font-size: 20px;
+  margin: 1em 0;
+`;
+
+const Introduction = styled.div`
+  width: 70%;
+`;
+
+const PosterSection = styled.div`
+  display: flex;
+  align-items: center;
+  width: 40%;
+`;
+
+const Poster = styled.img`
+  width: 300px;
+  height: 500px;
+`;
+
+
+const GET_MEDIA_QUERY = gql`
   query getMedia($id: Int!) {
     Media(id: $id) {
       id
@@ -12,26 +62,39 @@ const ASDF = gql`
         native
         userPreferred
       }
+      coverImage {
+        extraLarge
+      }
+      countryOfOrigin
+      description
     }
   }
 `;
 function Detail() {
   const { id } = useParams();
-  const { loading, data } = useQuery(ASDF, { variables: { id }});
+  const { loading, data } = useQuery(GET_MEDIA_QUERY, { variables: { id }});
   console.log(data);
 
   return (
-    <div>
-      {
-        loading
-          ? (
-            `Detail${id}`
-          )
-          : (
-            data.Media.title.english
-          )
-      }
-    </div>
+    <Wrapper>
+      <DescriptionWrapper>
+        <Description>
+          { loading && <Title>Loading</Title>}
+          <Title>
+            {data?.Media?.title.english}
+          </Title>
+          <SubDescription>
+            {data?.Media?.countryOfOrigin}
+          </SubDescription>
+          <Introduction>
+            {data?.Media?.description}
+          </Introduction>
+        </Description>
+      </DescriptionWrapper>
+      <PosterSection>
+        <Poster src={data?.Media?.coverImage.extraLarge} />
+      </PosterSection>
+    </Wrapper>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,76 @@ import {
   useQuery,
   gql,
 } from '@apollo/client';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 50vh;
+  background: linear-gradient(to right, #f03ab3, #e8781c);
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 2em;
+  font-size: 30px;
+  color: white;
+`;
+
+const Title = styled.span`
+  font-weight: bold;
+`;
+
+const SubTitle = styled.span`
+  font-size: 14px;
+`;
+
+const Loading = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+  font-weight: bold;
+  color: white;
+`;
+
+const AnimationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+`;
+
+const Animations = styled.div`
+  width: 60%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: -50px;
+`;
+
+const Animation = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  height: 250px;
+  margin: 0 10px;
+`;
+
+const Poster = styled.img`
+
+`;
+
+const LikeButton = styled.button`
+  width: 50px;
+  height: 30px;
+`;
 
 const QUERY = gql`
 {
@@ -42,17 +112,29 @@ function Home() {
 
   return (
     <div>
-      {
-        loading
-        ? "loading..."
-        : (
-          data.Page.mediaList.map(media => (
-            <div key={media.id} onClick={() => navigator(`/${media.media.id}`)}>
-              {media.media.title.english}
-            </div>
-          ))
-        )
-      }
+      <Header>
+        <TitleWrapper>
+          <Title>
+            Apollo Animations
+          </Title>
+          <SubTitle>
+            made with "Apollo Client" & "GraphQL"
+          </SubTitle>
+        </TitleWrapper>
+      </Header>
+      <AnimationWrapper>
+        <Animations>
+          { loading && <Loading>loading...</Loading> }
+          {
+            data?.Page?.mediaList.map(media => (
+              <Animation key={media.id} onClick={() => navigator(`/${media.media.id}`)}>
+                <Poster src={media.media.coverImage.extraLarge} />
+                <LikeButton>Like</LikeButton>
+              </Animation>
+            ))
+          }
+        </Animations>
+      </AnimationWrapper>
     </div>
   );
 }
